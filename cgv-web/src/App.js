a851c1ae9c15195c4757c56bb31ecf3f8af8ca97
uheaji <yoolbk@naver.com>
2021-03-10 00:03:06 +0900

    Footer.js 완성

--- cgv-web/src/App.js
+++ cgv-web/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <div>안녕</div>
+      <Footer />
     </>
   );
 }
