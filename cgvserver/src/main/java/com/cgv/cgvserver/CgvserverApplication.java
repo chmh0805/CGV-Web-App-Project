7f9683b05bd071f3c5c217cdeef8c08ff115fe29
chmh0805 <chmh0805@gmail.com>
2021-03-21 23:35:55 +0900

    v0.2.0 서버 yml파일 작성완료

---
+++ cgvserver/src/main/java/com/cgv/cgvserver/CgvserverApplication.java
@@ -0,0 +1,13 @@
+package com.cgv.cgvserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgvserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgvserverApplication.class, args);
+	}
+
+}
